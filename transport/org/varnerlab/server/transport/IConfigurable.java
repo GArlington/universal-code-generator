eab2603feebddf7cf56a3b6596c9036c9c7a9218
varner.jeffrey.d <varner.jeffrey.d@bd83be4a-0525-11df-b0f7-87f4a55fea09>
2010-01-19 20:45:27 +0000

    TRANSPORT

--- transport/org/varnerlab/server/transport/IConfigurable.java
+++
@@ -1,9 +0,0 @@
-package org.varnerlab.server.transport;
-
-public interface IConfigurable {
-
-	public void setProperty(String key,String value);
-	public void setProperty(Object key,Object value);
-	
-	
-}
