53b7347a2631116758057b2770ff225595ae9d3a
varner.jeffrey.d <varner.jeffrey.d@bd83be4a-0525-11df-b0f7-87f4a55fea09>
2010-01-19 21:31:48 +0000

    Editor

--- trunk/editor/org/varnerlab/universaleditor/gui/widgets/IVLWidgetComposite.java
+++
@@ -1,9 +0,0 @@
-package org.varnerlab.universaleditor.gui.widgets;
-
-/**
- *  Tagging interface to mark that this element has children
- *  @author J.Varner
- */
-public interface IVLWidgetComposite {
-}
-
