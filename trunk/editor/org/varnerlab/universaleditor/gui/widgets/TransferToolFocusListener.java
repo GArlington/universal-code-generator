ce7c2d8617557347ee804fee003dfdf2459e84af
varner.jeffrey.d <varner.jeffrey.d@bd83be4a-0525-11df-b0f7-87f4a55fea09>
2010-01-23 02:17:23 +0000

    Put in code to refresh the local file system jlist when the tool gains focus.

--- trunk/editor/org/varnerlab/universaleditor/gui/widgets/TransferToolFocusListener.java
+++ trunk/editor/org/varnerlab/universaleditor/gui/widgets/TransferToolFocusListener.java
@@ -8,19 +8,36 @@ package org.varnerlab.universaleditor.gui.widgets;
 // import -
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.util.Hashtable;
+import java.util.Vector;
 
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.text.*;
 
+import org.varnerlab.universaleditor.domain.UEditorSession;
 import org.varnerlab.universaleditor.gui.*;
+import org.varnerlab.universaleditor.service.FileSystemService;
 
 /**
  *
  * @author jeffreyvarner
  */
 public class TransferToolFocusListener implements InternalFrameListener {
-
+	// Class/instance -
+	private Hashtable _propTable = new Hashtable();
+	
+	public void setProperty(Object key,Object value)
+	{
+		_propTable.put(key, value);
+	}
+	
+	public Object getProperty(Object key)
+	{
+		return(_propTable.get(key));
+	}
+	
     public void internalFrameOpened(InternalFrameEvent e) {
         
     }
@@ -51,6 +68,24 @@ public class TransferToolFocusListener implements InternalFrameListener {
 
         // Set the on-icon -
         intFrame.setFrameIcon(icon);
+        
+        // Get the combo box -
+        JComboBox jcb = (JComboBox)getProperty("LOCAL_COMBOBOX");
+        File selected_file = (File)jcb.getSelectedItem();
+        
+        // Populate the JList w/the current directory -
+        Vector<File> _vecDir = new Vector();
+        
+        // Get the list model -
+        DefaultListModel _listModelJList1 = (DefaultListModel)getProperty("LOCAL_JLIST_MODEL");
+        _listModelJList1.clear();
+        
+        FileSystemService.getFileFromDir(selected_file, _vecDir);
+        int NUMBER_OF_ELEMENTS = _vecDir.size();
+        for (int pindex=0;pindex<NUMBER_OF_ELEMENTS;pindex++)
+        {
+            _listModelJList1.addElement(_vecDir.get(pindex));
+        }
 
     }
 
