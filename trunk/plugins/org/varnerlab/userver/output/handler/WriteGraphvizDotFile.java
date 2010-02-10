21680878b8f07c46499a01a72a29c17c8543040c
varner.jeffrey.d <varner.jeffrey.d@bd83be4a-0525-11df-b0f7-87f4a55fea09>
2010-02-10 11:45:56 +0000

    Gateway class to generate *.dot graphviz files.

---
+++ trunk/plugins/org/varnerlab/userver/output/handler/WriteGraphvizDotFile.java
@@ -0,0 +1,67 @@
+package org.varnerlab.userver.output.handler;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import org.sbml.libsbml.Model;
+import org.sbml.libsbml.Reaction;
+import org.varnerlab.server.transport.IOutputHandler;
+import org.varnerlab.server.transport.LoadXMLPropFile;
+import org.varnerlab.userver.language.handler.GraphvizModel;
+import org.varnerlab.userver.language.handler.OctaveCModel;
+import org.varnerlab.userver.language.handler.SBMLModelUtilities;
+
+public class WriteGraphvizDotFile implements IOutputHandler {
+	// Class/instance attributes -
+	private LoadXMLPropFile _xmlPropTree = null;
+	private int NUMBER_OF_SPECIES = 0; 
+    private int NUMBER_OF_RATES = 0;
+
+	public void setProperties(Hashtable prop) {
+	// TODO Auto-generated method stub
+	
+	}
+
+	public void setProperties(LoadXMLPropFile prop) {
+		_xmlPropTree = prop;
+	}
+
+	public void writeResource(Object object) throws Exception {
+		// Method attributes -
+        StringBuffer dot_buffer = new StringBuffer();
+        double[][] dblSTMatrix = null;
+        Vector<Reaction> vecReactions = new Vector<Reaction>();
+        GraphvizModel graphiz_model = new GraphvizModel();
+	
+        // Get the resource type (sbml model) -
+        Model model_wrapper = (Model)object;
+        
+        // Check to make sure all the reversible rates are 0,inf
+        SBMLModelUtilities.convertReversibleRates(model_wrapper,vecReactions);
+        
+        // set the model_wrapper -
+        graphiz_model.setModel(model_wrapper);
+        graphiz_model.setProperties(_xmlPropTree);
+        
+        // Ok, lets build the stoichiometric matrix -
+        NUMBER_OF_SPECIES = (int)model_wrapper.getNumSpecies(); 
+        NUMBER_OF_RATES = (int)vecReactions.size();
+        
+        // Initialize the stoichiometric matrix -
+        dblSTMatrix = new double[NUMBER_OF_SPECIES][NUMBER_OF_RATES];
+        
+        // Build the matrix -
+        SBMLModelUtilities.buildStoichiometricMatrix(dblSTMatrix, model_wrapper,vecReactions);
+        
+        // Construct the dot file -
+        graphiz_model.buildDotFileHeader(dot_buffer, model_wrapper, vecReactions);
+        graphiz_model.buildGraphizNodeList(dot_buffer, model_wrapper, vecReactions);
+        graphiz_model.buildGraphvizReactionList(dot_buffer,model_wrapper, vecReactions);
+        
+        // add the last line of the buffer -
+        dot_buffer.append("\n}\n");
+        
+        // Get the filename that we are going to dump to -
+        SBMLModelUtilities.dumpGeneralBufferToDisk(dot_buffer, _xmlPropTree);
+	}
+}
