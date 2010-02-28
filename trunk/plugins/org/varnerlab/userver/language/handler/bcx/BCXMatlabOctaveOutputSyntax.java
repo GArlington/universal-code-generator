9c9895c754a85129e87cee908df2fadcca1139a6
varner.jeffrey.d <varner.jeffrey.d@bd83be4a-0525-11df-b0f7-87f4a55fea09>
2010-02-28 22:23:34 +0000

    Integrated initial BCX code.Took Ryan's standalone code made changes to transport layer and the specific plugins to generate BCX code.

---
+++ trunk/plugins/org/varnerlab/userver/language/handler/bcx/BCXMatlabOctaveOutputSyntax.java
@@ -0,0 +1,223 @@
+package org.varnerlab.userver.language.handler.bcx;
+
+import java.util.Date;
+import java.util.Vector;
+
+import org.varnerlab.server.transport.IBCXOutputSyntax;
+import org.varnerlab.server.transport.LoadXMLPropFile;
+
+
+public class BCXMatlabOctaveOutputSyntax implements IBCXOutputSyntax {
+    
+	private LoadXMLPropFile _xmlPropTree = null;
+	
+	public void setProperties(LoadXMLPropFile prop) {
+        this._xmlPropTree = prop;
+	}
+    
+    public BCXMatlabOctaveOutputSyntax(){
+    }
+    
+      
+    public StringBuffer getExpHeaderBuffer(StringBuffer buffer, String expId, Date date, String cite){
+        buffer.append("function [Tsim, Xsim] = exp_"+expId+"(DF)\n");
+        buffer.append("% --Machine Made via BCX on "+date.toString()+"--\n");
+        buffer.append("% This code was designed to mimic experiment "+expId+".\n");
+        buffer.append("% Ref: "+cite+"\n");
+        buffer.append("% -----------------------------------------\n");
+        buffer.append("% Initailize vars\n");
+        buffer.append("\tnP = DF.NUMBER_PARAMETERS;\n");
+        buffer.append("\tnX = DF.NUMBER_OF_STATES;\n");
+        buffer.append("\tXsim = zeros(1,nX);\n");
+        buffer.append("\tTsim = [0];\n");
+        buffer.append("% Check on Inital Conditions\n");
+        return(buffer);
+    }
+    
+    public StringBuffer getSensExpHeaderBuffer(StringBuffer buffer, String expId, Date date, String cite){
+        buffer.append("function [Tsim, Xsim, Ssim] = expSens_"+expId+"(DF)\n");
+        buffer.append("% --Machine Made via BioChemExp on "+date.toString()+"--\n");
+        buffer.append("% This code was designed to mimic experiment "+expId+".\n");
+        buffer.append("% Ref: "+cite+"\n");
+        buffer.append("% There is an error here:\n");
+        buffer.append("% SolveAdjBal.m assumes S IC to be zero\n");
+        buffer.append("% This is not true when we call it multiple times in a row.\n");
+        buffer.append("% Need to change that program to take last points from previous run.\n");
+        buffer.append("% -----------------------------------------\n");
+        buffer.append("% Initailize vars\n");
+        buffer.append("\tnP = DF.NUMBER_PARAMETERS;\n");
+        buffer.append("\tnX = DF.NUMBER_OF_STATES;\n");
+        buffer.append("\tXsim = [];\n");
+        buffer.append("\tTsim = [];\n");
+        buffer.append("\tSsim = [];\n");
+        buffer.append("% Check on Inital Conditions\n");
+        return(buffer);
+    }
+    
+    public StringBuffer getAddStimulusBuffer(StringBuffer buffer,String paramId, String value, int basis){
+        buffer.append("\tDF.PARAMETER_VECTOR("+paramId+")=");
+        // check to see what kind of change we need to issue
+        if(basis==0){
+            // a relative change, and we assume the given value is a fraction
+            buffer.append("DF.PARAMETER_VECTOR("+paramId+")");
+            buffer.append("*"+value+";\n");
+        }
+        else if(basis==1){
+            // an absolute change
+            buffer.append("DF.PARAMETER_VECTOR("+paramId+")");
+            buffer.append("+"+value+";\n");
+        }
+        else if(basis==2){
+            // an absolute value
+            buffer.append(value+";\n");
+        }
+        return(buffer);
+    }
+    
+    public StringBuffer getExpSteadyStateBuffer(StringBuffer buffer){
+        buffer.append("% Run to steady state\n");
+        buffer.append("\tSSdelta = 1;\n");
+        buffer.append("\twhile(SSdelta>.001)\n");
+        double[] T = {0, 1000, 1000};
+        buffer = this.getExpRunBuffer(buffer,T);
+        T = new double[]{0,1,1};
+        buffer = this.getExpRunBuffer(buffer,T);
+        buffer.append("\tSStot = sum(X');\n");
+        buffer.append("\tSSdelta = (SStot(1)-SStot(2))/SStot(2);\n");
+        buffer.append("\tend\n");
+        return(buffer);
+        
+    }
+    
+     public StringBuffer getSensExpSteadyStateBuffer(StringBuffer buffer){
+        buffer.append("% Run to steady state\n");
+        buffer.append("\tSSdelta = 1;\n");
+        buffer.append("\twhile(SSdelta>.001)\n");
+        double[] T = {0, 1000, 1000};
+        buffer = this.getExpRunBuffer(buffer,T);
+        T = new double[]{0,1,1};
+        buffer = this.getExpRunBuffer(buffer,T);
+        buffer.append("\tSStot = sum(X');\n");
+        buffer.append("\tSSdelta = (SStot(1)-SStot(2))/SStot(2);\n");
+        buffer.append("\tend\n");
+        return(buffer);
+        
+    }
+    
+    public StringBuffer getExpRunBuffer(StringBuffer buffer, double[] T){
+        // lets look to see if the step is too big, cuz we always want to capture 
+        // somthing from each run
+        double Tstart = T[0];
+        double Tstop = T[1];
+        double Tstep = T[2];
+        double Td = Tstop - Tstart;
+        if(Tstep>Td){
+            Tstep = Td;
+        }
+                
+        // let change these to a string
+        String[] Ts = {String.valueOf(Tstart),
+                    String.valueOf(Tstop),String.valueOf(Tstep)};
+        // first we need to set up the itital conditions
+        buffer.append("\tDF.INITIAL_CONDITIONS=DF.PARAMETER_VECTOR(nP+1:end);\n");
+        
+        
+        // run the model
+        buffer.append("\t[T,X]=SolveMassBalances(@DataFile,"+Ts[0]+","+Ts[1]+","+
+                Ts[2]+",DF);\n");
+        // now set the IC in the Parameter Vectorfor next time 
+        buffer.append("\tDF.PARAMETER_VECTOR(nP+1:end) = X(end,:)';\n");
+        
+        return(buffer);
+    }
+    
+    public StringBuffer getSensExpRunBuffer(StringBuffer buffer, double[] T){
+        // lets look to see if the step is too big, cuz we always want to capture 
+        // somthing from each run
+        double Tstart = T[0];
+        double Tstop = T[1];
+        double Tstep = T[2];
+        double Td = Tstop - Tstart;
+        if(Tstep>Td){
+            Tstep = Td;
+        }
+                
+        // let change these to a string
+        String[] Ts = {String.valueOf(Tstart),
+                    String.valueOf(Tstop),String.valueOf(Tstep)};
+        // first we need to set up the itital conditions
+        buffer.append("\tDF.INITIAL_CONDITIONS=DF.PARAMETER_VECTOR(nP+1:end);\n");
+        // run the model
+        buffer.append("\t[T,X,S,estT]=SolveAdjBalC(@DataFile,"+Ts[0]+","+Ts[1]+","+
+                Ts[2]+",DF);\n");
+        // now set the IC in the Parameter Vectorfor next time 
+        buffer.append("\tDF.PARAMETER_VECTOR(nP+1:end) = X(end,:)';\n");
+        
+        return(buffer);
+    }
+    
+    public StringBuffer getExpUpdateResultsBuffer(StringBuffer buffer){
+        // override last endpoint with new initial point:
+        buffer.append("\tXsim = [Xsim(1:end-1,:);X];\n");
+        buffer.append("\tTsim = [Tsim(1:end-1);T'];\n");
+        buffer.append("\tSsim = [Ssim;S(1:end-nX,:)];\n");
+        return(buffer);
+    }
+    
+    public StringBuffer getSensExpUpdateResultsBuffer(StringBuffer buffer){
+        // override last endpoint with new initial point:
+        buffer.append("\tXsim = [Xsim(1:end-1,:);X];\n");
+        buffer.append("\tTsim = [Tsim(1:end-1);T'];\n");
+        return(buffer);
+    }
+    
+    public StringBuffer getExpFooterBuffer(StringBuffer buffer){
+        buffer.append("return;\n");
+        return(buffer);
+    }
+    
+    public StringBuffer getSensExpFooterBuffer(StringBuffer buffer){
+        buffer.append("return;\n");
+        return(buffer);
+    }
+    
+    public void writeExpBuffer(StringBuffer buffer, String expId) throws Exception {
+        CUtil.write(new String("exp_"+expId+".m"),buffer,_xmlPropTree);
+    }
+    
+    public void writeSensExpBuffer(StringBuffer buffer, String expId) throws Exception {
+        CUtil.write(new String("expSen_"+expId+".m"),buffer,_xmlPropTree);
+    }
+    
+    public StringBuffer getMSEGraphFntHeader(StringBuffer buffer,Date date){
+        
+    	// update the name of the grapher file -
+    	buffer.append("function [MSE] = grapher(DF)\n");
+    	
+        buffer.append("% --Machine Made via BioChemExp on "+date.toString()+"--\n");
+        buffer.append("% This code was designed to graph relative\n");
+        buffer.append("% simulation and experimental results\n");
+        buffer.append("% -----------------------------------------\n");
+        // set up the total data variable to calculate the MSE
+        buffer.append("\t% create a place to save all data for MSE\n");
+        buffer.append("\ttotData = [];\n");
+        return(buffer);
+    }
+    
+    public StringBuffer getMSEFntRun(StringBuffer buffer,Vector expList){
+        buffer.append("\t% Run all of the experiments\n");
+        for(int i=0;i<expList.size();++i)
+        {
+            Exp tmpExp = (Exp)expList.get(i);
+        	String id = tmpExp.expId;
+            buffer.append("\t[T"+id+", X"+id+"] = exp_"+id+"(DF);\n");
+        }
+        return(buffer);
+    }
+
+
+	public StringBuffer getMSEFntRun(StringBuffer buffer, Object[] expList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ trunk/plugins/org/varnerlab/userver/language/handler/bcx/Data.java
@@ -0,0 +1,121 @@
+package org.varnerlab.userver.language.handler.bcx;
+
+public class Data {
+
+	 /** Data id, a string that unequlley defines this data set */
+    public String dataId = "";
+    /** Species ID, name of species */
+    public String speciesId;
+    /** Free species or complexed */
+    public boolean freeSpecies;
+    /** Array of speciesIndexs to identify all of the species to be summed to
+     *  obtain the observable.  Currentlly we are using the species index
+     *  associated with the VLab Universal software.  
+     *
+     */
+    public int[] speciesIndex = new int[0];
+    /** Array of coefficents that multiply corisponding species in speciesIndex
+     * to obtain the sum for the observable, the stochiometry.
+     */
+    public int[] speciesCoef = new int[0];
+    /** Value for single measurment of the given observable */
+    public double value;
+    /** Time that corisponds to the value of the
+     * given observable */
+    public double time;
+    /** Error in mesurment, this houses the error in the corisponding value */
+    public double valueErr;
+    /** This is the data group ID which is used to to determine other 
+     *  data sets which are relative to this trajectory.  "NA" is used to 
+     *  indicate the fact that these values are absolute, and not relative values.
+     *  "self" or the blank set, "", is used to indicate that these values are 
+     *  only relitive to eachother and will not be compared to any other datasets 
+     */ 
+    public String groupId = "";
+    /** The citation for the data. */
+    public String cite = "";
+    
+    /** Creates a new instance of Data 
+     *  by defult assume this is only self relative.
+     */
+    public Data() {
+    }
+    /** Sets the data id, which is a string that unequlley defines this 
+     *  data set.
+     *  @param dataId       String, unique data set identifier
+     */
+    public void setDataId(String dataId){
+        this.dataId = dataId;
+    }
+    /** Sets the actual name of the species
+     *
+     * @param speciesId     String, name of species
+     */
+    public void setSpeciesId(String speciesId){
+        this.speciesId = speciesId;
+    }
+    /** Sets the free species marker
+     *
+     * @param free      Boolean, this will be true if we are only considering free species 
+     */
+    public void setFree(boolean free){
+        this.freeSpecies = free;
+    }
+    /** Add a species to summation list for the observable.  An observable 
+     *  can be a summation over diffrent species for cases when total protein
+     *  X is required given that X has bound, free, modified, and/or unmodifed
+     *  forms.
+     *  @param speciesIndex        int, Currentlly we are using the species index associated with the VLab Universal software.
+     *  @param speciesCoef      int, coefficents that multiply corisponding species in speciesIndex to obtain the sum for the observable, the stochiometry
+     */
+    public void addSpeciesIndex(int speciesIndex, int speciesCoef){
+        this.speciesCoef = CUtil.addElement(this.speciesCoef,speciesCoef);
+        this.speciesIndex = CUtil.addElement(this.speciesIndex,speciesIndex);
+    }
+    /** Add a species to summation list for the observable.  An observable 
+     *  can be a summation over diffrent species for cases when total protein
+     *  X is required given that X has bound, free, modified, and/or unmodifed
+     *  forms.  This assumes the stoch is unity.
+     *  @param speciesIndex        int, Currentlly we are using the species index associated with the VLab Universal software.
+     */
+    public void addSpeciesIndex(int speciesIndex){
+        addSpeciesIndex(speciesIndex,1);
+    }
+    /** Add a value to the list of time course values for this data set.
+     *  @param value        double, value of observable
+     *  @param time         double, absolute model time at which value is observed
+     *  @param valueErr     double, error in value (same units as value)
+     */
+    public void addValue(double time, double value, double valueErr){
+        this.value = value;
+        this.time = time;
+        this.valueErr = valueErr;
+    }
+    /** Add a value to the list of time course values for this data set.
+     *  Assumes error of 10%
+     *  @param value        double, value of observable
+     *  @param time         double, absolute model time at which value is observed
+     */
+    public void addValue( double time, double value){
+        addValue(time, value, .1*value);
+    }
+    /** Set the group ID, which is used to to determine other 
+     *  data sets which are relative to this trajectory.  "NA" is used to 
+     *  indicate the fact that these values are absolute, and not relative values.
+     *  "self" or the blank set, "", is used to indicate that these values are 
+     *  only relitive to eachother and will not be compared to any other datasets
+     *  @param groupId      String, a unique relative dataset identifier
+     */
+    public void setGroupId(String groupId){
+        this.groupId = groupId;
+    }
+    /** Set the citation from which this data set came from 
+     *  @param cite     Stirng, generic dscription of citation
+     */
+    public void setCite(String cite){
+        //ATTENTION may want to add some structure here to make better use of this feild!!
+        // maybe a citation object that has feild similar to that of latex???
+        this.cite = cite;
+    }
+	
+}
