d99d260fc5b207159f88493f3136be51dd767789
varner.jeffrey.d <varner.jeffrey.d@bd83be4a-0525-11df-b0f7-87f4a55fea09>
2010-02-28 22:21:27 +0000

    Updated to include BCX code.

---
+++ transport_server/org/varnerlab/server/transport/IBCXOutputSyntax.java
@@ -0,0 +1,23 @@
+package org.varnerlab.server.transport;
+
+import java.util.*;
+
+public interface IBCXOutputSyntax {
+
+	public StringBuffer getExpHeaderBuffer(StringBuffer buffer, String expId, Date date, String cite);
+    public StringBuffer getSensExpHeaderBuffer(StringBuffer buffer, String expId, Date date, String cite);
+    public StringBuffer getAddStimulusBuffer(StringBuffer buffer,String paramId, String value, int basis);
+    public StringBuffer getExpSteadyStateBuffer(StringBuffer buffer);
+    public StringBuffer getSensExpSteadyStateBuffer(StringBuffer buffer);
+    public StringBuffer getExpRunBuffer(StringBuffer buffer, double[] T);
+    public StringBuffer getSensExpRunBuffer(StringBuffer buffer, double[] T);
+    public StringBuffer getExpUpdateResultsBuffer(StringBuffer buffer);
+    public StringBuffer getSensExpUpdateResultsBuffer(StringBuffer buffer);
+    public StringBuffer getExpFooterBuffer(StringBuffer buffer);
+    public StringBuffer getSensExpFooterBuffer(StringBuffer buffer);
+    public void writeExpBuffer(StringBuffer buffer, String expId) throws Exception;
+    public void writeSensExpBuffer(StringBuffer buffer, String expId) throws Exception;
+    public StringBuffer getMSEGraphFntHeader(StringBuffer buffer,Date date);
+    public StringBuffer getMSEFntRun(StringBuffer buffer,Object[] expList);
+    public StringBuffer getMSEFntRun(StringBuffer buffer,Vector expList);
+}
