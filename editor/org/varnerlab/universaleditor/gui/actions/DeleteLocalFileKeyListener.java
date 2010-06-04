cf36d1b4d20c689b778a98c8df10637ae07e7a63
varner.jeffrey.d <varner.jeffrey.d@bd83be4a-0525-11df-b0f7-87f4a55fea09>
2010-06-04 21:30:47 +0000

    Notify the user when a file is going to be deleted.

--- editor/org/varnerlab/universaleditor/gui/actions/DeleteLocalFileKeyListener.java
+++ editor/org/varnerlab/universaleditor/gui/actions/DeleteLocalFileKeyListener.java
@@ -8,24 +8,36 @@ package org.varnerlab.universaleditor.gui.actions;
 import java.awt.Component;
 import java.awt.KeyboardFocusManager;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 
 import org.varnerlab.universaleditor.gui.FileTransferTool;
+import org.varnerlab.universaleditor.gui.Launcher;
+import org.varnerlab.universaleditor.gui.ModelCodeGeneratorFileEditor;
+import org.varnerlab.universaleditor.gui.widgets.SheetDialogFrame;
+import org.w3c.dom.Document;
 
 /**
  *
  * @author jeffreyvarner
  */
-public class DeleteLocalFileKeyListener implements KeyListener {
+public class DeleteLocalFileKeyListener implements KeyListener,ActionListener,PropertyChangeListener  {
 	// Class/instance attributes -
 	private JList _jList= null;
 	private File[] selectedFileArr;
 	private FileTransferTool windowFrame;
+	private JOptionPane optionPane;
+	private SheetDialogFrame _frame;
+	private File _fileLocal = null;
 	
 	private Component focusedComponent = null;
     private KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
@@ -49,22 +61,75 @@ public class DeleteLocalFileKeyListener implements KeyListener {
 	        focusedComponent = manager.getFocusOwner();
 	        windowFrame = (FileTransferTool)focusedComponent.getFocusCycleRootAncestor();
         	
+	        // Ok, so we need to add a dialog to make sure it is ok to delete what ever I'm about to delete
+	        
+	        
         	// Ok, so when I get here I'm going to delete local files?
     		Object[] objArr = _jList.getSelectedValues();
     		int NITEMS = objArr.length;
     		for (int index=0;index<NITEMS;index++)
     		{
     			File tmpFile = (File)objArr[index];
-    			tmpFile.delete();
+    			if (tmpFile.isFile())
+    			{
+    				// what file is selected -
+    				_fileLocal = tmpFile;
+    				
+    				// Pop the dialog?
+    				String tmp = tmpFile.getPath();
+    				String strMessage = "Delete \""+tmpFile.getName()+"\"?\nDo you really want to delete this?\n" +
+    						"Deleting this file or directory can not be undone.";
+    				optionPane = new JOptionPane(strMessage,JOptionPane.QUESTION_MESSAGE,JOptionPane.YES_NO_OPTION);
+    				optionPane.addPropertyChangeListener(this);
+    				JDialog jDialog = optionPane.createDialog(_frame,"Spank");
+    				windowFrame.showJDialogAsSheet(jDialog);
+    			}
     		}
-    		
-    		// update the GUI =
-    		windowFrame.updateSelectedDirectory();
         }
     }
 
     public void keyReleased(KeyEvent e) {
         System.out.println("Hey now -- ");
     }
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void propertyChange(PropertyChangeEvent evt) {
+		
+		if (evt.getPropertyName().equals (JOptionPane.VALUE_PROPERTY)) 
+		{
+					
+			FileTransferTool windowFrame = (Launcher.getInstance()).getFileTransferToolRef();
+			int value = ((Integer)optionPane.getValue()).intValue();
+			
+			if (value==0)
+			{
+			
+				try {				
+					// delete the file -
+					_fileLocal.delete();
+					
+					// update the GUI =
+		    		windowFrame.updateSelectedDirectory();
+				}
+				catch (Exception error)
+				{
+					error.printStackTrace();
+				}
+				finally
+				{
+					windowFrame.hideSheet();
+				}
+			}
+			else
+			{
+				windowFrame.hideSheet();
+			}
+		}
+	}
 
 }
