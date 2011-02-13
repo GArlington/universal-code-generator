eee0a52d882d6ac98eca9e4c5cbe5e9c33fa61e7
varner.jeffrey.d@gmail.com <varner.jeffrey.d@gmail.com@bd83be4a-0525-11df-b0f7-87f4a55fea09>
2011-02-13 16:04:50 +0000

    Implemented measurement selection vector from the data file. Defaults is all the states

--- trunk/plugins/src/org/varnerlab/userver/language/handler/SBMLModelUtilities.java
+++ trunk/plugins/src/org/varnerlab/userver/language/handler/SBMLModelUtilities.java
@@ -287,6 +287,10 @@ public class SBMLModelUtilities {
 	        datafile.append("end;\n");
 	        datafile.append("\n");
 	        
+	        // Populate the measurement selection matrix -
+	        datafile.append("% Initialize tehg measurement selection matrix. Default is the identity matrix \n");
+	        datafile.append("MEASUREMENT_MATRIX = eye(NSTATES,NSTATES);\n");
+	        
 	        datafile.append("% =========== DO NOT EDIT BELOW THIS LINE ==============\n");
 	        datafile.append("DF.STOICHIOMETRIC_MATRIX=S;\n");
 	        datafile.append("DF.RATE_CONSTANT_VECTOR=k;\n");
@@ -294,6 +298,7 @@ public class SBMLModelUtilities {
 	        datafile.append("DF.NUMBER_PARAMETERS=NPARAMETERS;\n");
 	        datafile.append("DF.NUMBER_OF_STATES=NSTATES;\n");
 	        datafile.append("DF.PARAMETER_VECTOR=kV;\n");
+	        datafile.append("DF.MEASUREMENT_SELECTION_MATRIX = MEASUREMENT_MATRIX;\n");
 	        datafile.append("% ======================================================\n");
 	        datafile.append("return;\n");
 		}
@@ -423,6 +428,10 @@ public class SBMLModelUtilities {
 	        datafile.append("end;\n");
 	        datafile.append("\n");
 	        
+	        // Populate the measurement selection matrix -
+	        datafile.append("% Initialize tehg measurement selection matrix. Default is the identity matrix \n");
+	        datafile.append("MEASUREMENT_INDEX_VECTOR = [1:NSTATES];\n");
+	        
 	        datafile.append("% =========== DO NOT EDIT BELOW THIS LINE ==============\n");
 	        datafile.append("DF.STOICHIOMETRIC_MATRIX=S;\n");
 	        datafile.append("DF.RATE_CONSTANT_VECTOR=k;\n");
@@ -430,6 +439,7 @@ public class SBMLModelUtilities {
 	        datafile.append("DF.NUMBER_PARAMETERS=NPARAMETERS;\n");
 	        datafile.append("DF.NUMBER_OF_STATES=NSTATES;\n");
 	        datafile.append("DF.PARAMETER_VECTOR=kV;\n");
+	        datafile.append("DF.MEASUREMENT_SELECTION_VECTOR = MEASUREMENT_INDEX_VECTOR;\n");
 	        datafile.append("% ======================================================\n");
 	        datafile.append("return;\n");
 		}
