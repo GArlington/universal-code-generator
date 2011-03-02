43c598fe88d1216f46787d48a6d78d68bead5d96
varner.jeffrey.d <varner.jeffrey.d@gmail.com>
2011-03-02 13:42:19 +0000

    Created dFBA package to hold different langauge handlers for dFBA

---
+++ plugins/src/org/varnerlab/userver/output/handler/dfba/WriteOctaveMDFBAFiles.java
@@ -0,0 +1,35 @@
+package org.varnerlab.userver.output.handler.dfba;
+
+import java.util.Hashtable;
+import java.util.logging.Logger;
+
+import org.varnerlab.server.localtransportlayer.IOutputHandler;
+import org.varnerlab.server.localtransportlayer.XMLPropTree;
+
+public class WriteOctaveMDFBAFiles implements IOutputHandler {
+
+	@Override
+	public void setLogger(Logger log) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setProperties(Hashtable prop) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setProperties(XMLPropTree prop) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void writeResource(Object object) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
