01e45c9b39deea232ff6adbfba9ce48df3627355
varner.jeffrey.d <varner.jeffrey.d@gmail.com>
2011-03-09 14:59:55 +0000

    Completed extracellular balance implementation

--- plugins/src/org/varnerlab/userver/output/handler/dfba/DFBAOctaveMMatlabMModelUtilities.java
+++ plugins/src/org/varnerlab/userver/output/handler/dfba/DFBAOctaveMMatlabMModelUtilities.java
@@ -1,5 +1,6 @@
 package org.varnerlab.userver.output.handler.dfba;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Vector;
 
@@ -90,7 +91,7 @@ public class DFBAOctaveMMatlabMModelUtilities {
         	buffer.append(species.getId());
         	buffer.append("\t = \t");
         	buffer.append("x(");
-        	buffer.append(index);
+        	buffer.append(index+1);
         	buffer.append(",1);\n");
         }
         buffer.append("\n");
@@ -98,9 +99,10 @@ public class DFBAOctaveMMatlabMModelUtilities {
         buffer.append("% ---------------------------------------- \n");
         buffer.append("% Compute the rates -\n");
         buffer.append("% ---------------------------------------- \n");
+        
+        // Build the rates -
         formulateReactionKinetics(model_wrapper,buffer,vecSpecies,xmlPropTree);
         
-	    
         // return -
         buffer.append("\n");
         buffer.append("return;\n");
@@ -115,7 +117,7 @@ public class DFBAOctaveMMatlabMModelUtilities {
 		
 		// Ok, get the list of reactions that are extracellular *and* that we are modeling kinetically -
         // We are going to do this by do a xpath hit on the *actual* xmltree 
-        String strXPath = ".//RequiredSBMLInformation/listOfKineticExtracellularReactions/reaction/@id";
+        String strXPath = ".//RequiredSBMLInformation/listOfKineticExtracellularReactions/reaction/@name";
         NodeList nodeList = (NodeList)xpath.evaluate(strXPath,xmlDoc,XPathConstants.NODESET);
         int NUMBER_OF_REACTIONS= nodeList.getLength();
         int global_counter = 0;
@@ -135,7 +137,7 @@ public class DFBAOctaveMMatlabMModelUtilities {
 			String strNodeID = tmpIDNode.getNodeValue();
 			
 			// Ok, so now we need to get a list of species for this rate -
-			String strXPathSpeciesRefs = ".//RequiredSBMLInformation/listOfKineticExtracellularReactions/reaction[@id='"+strNodeID+"']/listOfReactants/speciesReference/@species";
+			String strXPathSpeciesRefs = ".//RequiredSBMLInformation/listOfKineticExtracellularReactions/reaction[@name='"+strNodeID+"']/listOfReactants/speciesReference/@species";
 			NodeList speciesNodeList = (NodeList)xpath.evaluate(strXPathSpeciesRefs,xmlDoc,XPathConstants.NODESET);
 			int NUMBER_OF_SPECIES = speciesNodeList.getLength();
 			for (int species_index=0;species_index<NUMBER_OF_SPECIES;species_index++)
@@ -166,7 +168,11 @@ public class DFBAOctaveMMatlabMModelUtilities {
 			}
 			
 			// Ok, so I should have a rate here - close it out 
-			buffer.append(";\n");
+			buffer.append(";\t % ");
+			buffer.append(index+1);
+			buffer.append(" \t");
+			buffer.append(strNodeID);
+			buffer.append("\n");
 		}
 	}
 	
@@ -235,13 +241,177 @@ public class DFBAOctaveMMatlabMModelUtilities {
 		}
 	}
 	
+	private static void formulateUptakeVector(Model model_wrapper,StringBuffer buffer,Vector<Species> vecSpecies,XMLPropTree xmlPropTree) throws Exception
+	{
+		// Method attributes --
+		Document xmlDoc = (Document)xmlPropTree.getResource(null);
+		XPathFactory  xpFactory = XPathFactory.newInstance();
+		XPath xpath = xpFactory.newXPath();
+		ArrayList<String> globalSpeciesList = new ArrayList<String>();
+		
+		int NUMBER_EXTERNAL_SPECIES = vecSpecies.size();
+		buffer.append("qV = zeros(");
+		buffer.append(NUMBER_EXTERNAL_SPECIES);
+		buffer.append(",1);\n");
+		
+		for (int external_species_index=0;external_species_index<NUMBER_EXTERNAL_SPECIES;external_species_index++)
+		{
+		
+			// Get the name of the extracellular species -
+			Species external_species = vecSpecies.get(external_species_index);
+			String strExternalSpeciesName = external_species.getId();
+			
+		
+			buffer.append("qV(");
+			buffer.append(external_species_index+1);
+			buffer.append(",1) \t = \t B*(");
+		
+			// Ok, get the list of reactions that are extracellular *and* that we are modeling kinetically -
+			// We are going to do this by do a xpath hit on the *actual* xmltree 
+			String strXPath = ".//RequiredSBMLInformation/listOfKineticExtracellularReactions/reaction/@id";
+			NodeList nodeList = (NodeList)xpath.evaluate(strXPath,xmlDoc,XPathConstants.NODESET);
+			int NUMBER_OF_REACTIONS= nodeList.getLength();
+			int global_counter = 0;
+			for (int index = 0;index<NUMBER_OF_REACTIONS;index++)
+			{
+				// Get the current reaction id -
+				Node tmpIDNode = nodeList.item(index);
+			
+				// Get the id string -
+				String strNodeID = tmpIDNode.getNodeValue();
+			
+				// Ok, so now we need to get a list of species for this rate -
+				String strXPathSpeciesRefs = ".//RequiredSBMLInformation/listOfKineticExtracellularReactions/reaction[@id='"+strNodeID+"']/listOfReactants/speciesReference/@species";
+				NodeList speciesNodeList = (NodeList)xpath.evaluate(strXPathSpeciesRefs,xmlDoc,XPathConstants.NODESET);
+				ArrayList<String> speciesListReactants = new ArrayList<String>();
+				ArrayList<String> stCoeffListReactants = new ArrayList<String>();
+				int NUMBER_OF_SPECIES = speciesNodeList.getLength();
+				for (int species_index=0;species_index<NUMBER_OF_SPECIES;species_index++)
+				{
+					// Get the species node -
+					Node speciesNode = speciesNodeList.item(species_index);
+					String strSpeciesName = speciesNode.getNodeValue();
+					
+					if (!strSpeciesName.isEmpty())
+					{
+					
+						// Add name to list -
+						speciesListReactants.add(strSpeciesName);
+						
+						if (!globalSpeciesList.contains(strSpeciesName))
+						{
+							globalSpeciesList.add(strSpeciesName);
+						}
+					
+						// Get the stoichiometric coefficients -
+						String strSTCoeff =  ".//RequiredSBMLInformation/listOfKineticExtracellularReactions/reaction[@id='"+strNodeID+"']/listOfReactants/speciesReference[@species='"+strSpeciesName+"']/@stoichiometry";
+						Node stCoeffNode = (Node)xpath.evaluate(strSTCoeff,xmlDoc,XPathConstants.NODE);
+						String strStrCoeff = stCoeffNode.getNodeValue();
+					
+						// Add st coeff to list -
+						stCoeffListReactants.add(strStrCoeff);
+					}
+				}
+				
+				// Ok, so now we need to get a list of species for this rate -
+				String strXPathSpeciesRefsProducts = ".//RequiredSBMLInformation/listOfKineticExtracellularReactions/reaction[@id='"+strNodeID+"']/listOfProducts/speciesReference/@species";
+				NodeList speciesNodeListProducts = (NodeList)xpath.evaluate(strXPathSpeciesRefsProducts,xmlDoc,XPathConstants.NODESET);
+				ArrayList<String> speciesListProducts = new ArrayList<String>();
+				ArrayList<String> stCoeffListProducts = new ArrayList<String>();
+				int NUMBER_OF_PRODUCTS = speciesNodeListProducts.getLength();
+				for (int species_index=0;species_index<NUMBER_OF_PRODUCTS;species_index++)
+				{
+					// Get the species node -
+					Node speciesNode = speciesNodeListProducts.item(species_index);
+					String strSpeciesName = speciesNode.getNodeValue();
+					
+					if (!strSpeciesName.isEmpty())
+					{
+						// Add name to list -
+						speciesListProducts.add(strSpeciesName);
+						
+						if (!globalSpeciesList.contains(strSpeciesName))
+						{
+							globalSpeciesList.add(strSpeciesName);
+						}
+					
+						// Get the stoichiometric coefficients -
+						String strSTCoeff =  ".//RequiredSBMLInformation/listOfKineticExtracellularReactions/reaction[@id='"+strNodeID+"']/listOfProducts/speciesReference[@species='"+strSpeciesName+"']/@stoichiometry";
+						Node stCoeffNode = (Node)xpath.evaluate(strSTCoeff,xmlDoc,XPathConstants.NODE);
+						String strStrCoeff = stCoeffNode.getNodeValue();
+					
+						// Add st coeff to list -
+						stCoeffListProducts.add(strStrCoeff);
+					}
+				}
+				
+				// Ok, when I get here I have the reactants for this reaction -
+				// Is my externalSpecies involved in this reaction?
+				if (speciesListReactants.contains(strExternalSpeciesName))
+				{
+					// Get the index -
+					int local_index = speciesListReactants.indexOf(strExternalSpeciesName);
+					
+					// Get the stcoeff value -
+					String strLocalCoeffVal = stCoeffListReactants.get(local_index);
+					
+					// Ok, so I need to add this reaction to the list -
+					buffer.append("-");
+					buffer.append(strLocalCoeffVal);
+					buffer.append("*");
+					buffer.append("rV(");
+					buffer.append(index+1);
+					buffer.append(",1)");	
+				}
+				else if (speciesListProducts.contains(strExternalSpeciesName))
+				{
+					// Get the index -
+					int local_index = speciesListProducts.indexOf(strExternalSpeciesName);
+					
+					// Get the stcoeff value -
+					String strLocalCoeffVal = stCoeffListProducts.get(local_index);
+					
+					// Ok, so I need to add this reaction to the list -
+					buffer.append(strLocalCoeffVal);
+					buffer.append("*");
+					buffer.append("rV(");
+					buffer.append(index+1);
+					buffer.append(",1)");	
+				}
+				else
+				{
+					// Ok, so If I get here, then I have an external variable that is not involved in any reactions -
+				}
+			}
+			
+			
+			// Ok, when I get here I have gone through all the reactions - I have may have a malfunction??
+			if (!globalSpeciesList.contains(strExternalSpeciesName))
+			{
+				// We have a problem -- we have an external species *not* involved in any reactions?
+				buffer.append("0); \t % You need to check this -- ");
+				buffer.append(strExternalSpeciesName);
+				buffer.append(" is an extracellular species that is NOT involved in any uptake/production reactions?\n");
+				
+			}
+			else
+			{
+			
+				// close out and go around again -
+				buffer.append(");\n");
+			}
+		}
+	}
+	
 	public static void buildExtracellularMassBalances(Model model_wrapper,StringBuffer buffer,Vector<Species> vecSpecies,XMLPropTree xmlPropTree) throws Exception
 	{
 		// Method attributes -
 		Hashtable<String,String> pathTableMassbalanceFile = xmlPropTree.buildFilenameBlockDictionary("ExtracellularMassBalanceFunction");
-	
+		Hashtable<String,String> pathTableKineticsFile = xmlPropTree.buildFilenameBlockDictionary("KineticsFunction");
+		
 		// Get the extracellular massbalance file name -
 		String strMassBalanceFunctionName = pathTableMassbalanceFile.get("FUNCTION_NAME");
+		String strKineticsFunctionName = pathTableKineticsFile.get("FUNCTION_NAME");
 		
 		// Populate the buffer -
 		int NUMBER_OF_EXTRACELLULAR_SPECIES = vecSpecies.size();
@@ -251,13 +421,34 @@ public class DFBAOctaveMMatlabMModelUtilities {
 		buffer.append("(x,t)\n");
         buffer.append("\n");
         
+        buffer.append("% ----------------------------------------------------------------------\n");
+        buffer.append("% ");
+        buffer.append(strMassBalanceFunctionName);
+        buffer.append(".m was generated using the UNIVERSAL code generator system.\n");
+        buffer.append("% Username: ");
+        buffer.append(xmlPropTree.getProperty(".//Model/@username"));
+        buffer.append("\n");
+        buffer.append("% Type: ");
+        buffer.append(xmlPropTree.getProperty(".//Model/@type"));
+        buffer.append("\n");
+        buffer.append("% Version: ");
+        buffer.append(xmlPropTree.getProperty(".//Model/@version"));
+        buffer.append("\n");
+        buffer.append("% \n");
+        buffer.append("% Arguments: \n");
+        buffer.append("% x\t - \t state vector \n");
+        buffer.append("% t\t - \t current time \n");
+        buffer.append("% DXDT\t - \t right-hand side of the extracellular mass balances \n");
+        buffer.append("% ----------------------------------------------------------------------\n");
+        buffer.append("\n");
+        
         // call the globals -
         buffer.append("% ---------------------------------------- \n");
         buffer.append("% Declare the global variables -\n");
         buffer.append("% ---------------------------------------- \n");
         buffer.append("global DF_NETWORK;\n");
         buffer.append("global DF_KINETICS;\n");
-        buffer.append("global SIM;\n");
+        buffer.append("global LP_SOLUTION_GROWTH_RATE;\n");
         buffer.append("\n");
         
         buffer.append("% ---------------------------------------- \n");
@@ -271,9 +462,14 @@ public class DFBAOctaveMMatlabMModelUtilities {
         buffer.append("FOUT = interp1(FARR(:,1),FARR(:,3),t);\n");
         buffer.append("\n");
         
-        
-        
-        
+        buffer.append("% ---------------------------------------- \n");
+        buffer.append("% Call the kinetics function -\n");
+        buffer.append("% ---------------------------------------- \n");
+        buffer.append("rV = ");
+        buffer.append(strKineticsFunctionName);
+        buffer.append("(x,kV);\n");
+        buffer.append("\n");
+          
         buffer.append("% ---------------------------------------- \n");
         buffer.append("% Alias the x's -\n");
         buffer.append("% ---------------------------------------- \n");
@@ -303,6 +499,12 @@ public class DFBAOctaveMMatlabMModelUtilities {
         buffer.append("x(");
     	buffer.append(NUMBER_OF_EXTRACELLULAR_SPECIES+2);
     	buffer.append(",1);\n");
+    	buffer.append("\n");
+    	
+    	buffer.append("% ---------------------------------------- \n");
+        buffer.append("% Formulate qV -\n");
+        buffer.append("% ---------------------------------------- \n");
+        formulateUptakeVector(model_wrapper,buffer,vecSpecies,xmlPropTree);
 		
     	buffer.append("\n");
     	buffer.append("% ---------------------------------------- \n");
@@ -326,7 +528,9 @@ public class DFBAOctaveMMatlabMModelUtilities {
         	buffer.append(species.getId());
         	buffer.append(" - ");
         	buffer.append(species.getId());
-        	buffer.append("*(1/V)*(FIN-FOUT) + qV;\n");
+        	buffer.append("*(1/V)*(FIN-FOUT) + qV(");
+        	buffer.append(index+1);
+        	buffer.append(",1);\n");
         	
         	// Ok, we need to formulate the "bio" part of the problem -
         }
@@ -343,7 +547,7 @@ public class DFBAOctaveMMatlabMModelUtilities {
         buffer.append(" - (FOUT/V)*B");
     	buffer.append(" - ");
     	buffer.append("B");
-    	buffer.append("*(1/V)*(FIN-FOUT)\n");
+    	buffer.append("*(1/V)*(FIN-FOUT)+B*LP_SOLUTION_GROWTH_RATE;\n");
         
         // return -
         buffer.append("\n");
